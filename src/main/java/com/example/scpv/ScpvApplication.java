b35cc52c27085a196a4187587a1040d6afb0e547
Gustavo <gu.alvesslv@gmail.com>
2025-06-09 14:26:41 -0300

    :tada: Inicia projeto com estrutura de pastas

---
+++ src/main/java/com/example/scpv/ScpvApplication.java
@@ -0,0 +1,13 @@
+package com.example.scpv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScpvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScpvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scpv/ScpvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scpv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScpvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
