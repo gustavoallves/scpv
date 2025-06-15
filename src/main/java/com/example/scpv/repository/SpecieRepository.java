527f0a97b7b4189baec8280eb8ae350ea6ca838b
Gustavo <gu.alvesslv@gmail.com>
2025-06-14 22:42:48 -0300

    :sparkles: feat: Adiciona SpecieRepository ao projeto Criacao de repository para entidade Specie utilizando JpaRepository

---
+++ src/main/java/com/example/scpv/repository/SpecieRepository.java
@@ -0,0 +1,7 @@
+package com.example.scpv.repository;
+
+import com.example.scpv.entity.Specie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpecieRepository extends JpaRepository<Specie, Long> {
+}
