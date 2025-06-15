7dfcda81dab03e21bbbc8ad6555bbf985204eb52
Gustavo <gu.alvesslv@gmail.com>
2025-06-15 19:50:58 -0300

    :recycle: refactor: Refatora DTO, Entity e Controller para uso de Lombok Substitui getters, setters e construtores em SpecieDTO, SpecieEntity e SpecieController com Lombok.

--- src/main/java/com/example/scpv/specie/dto/SpecieDTO.java
+++ src/main/java/com/example/scpv/specie/dto/SpecieDTO.java
@@ -1,7 +1,16 @@
 package com.example.scpv.specie.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class SpecieDTO {
 
     private Long id;
@@ -10,53 +19,4 @@ public class SpecieDTO {
     private BigDecimal netSalesPrice;
     private BigDecimal grossPurchasePrice;
 
-    public SpecieDTO() {
-    }
-
-    public SpecieDTO(String speciesName, BigDecimal lossPercentage, BigDecimal netSalesPrice, BigDecimal grossPurchasePrice) {
-        this.speciesName = speciesName;
-        this.lossPercentage = lossPercentage;
-        this.netSalesPrice = netSalesPrice;
-        this.grossPurchasePrice = grossPurchasePrice;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getSpeciesName() {
-        return speciesName;
-    }
-
-    public void setSpeciesName(String speciesName) {
-        this.speciesName = speciesName;
-    }
-
-    public BigDecimal getLossPercentage() {
-        return lossPercentage;
-    }
-
-    public void setLossPercentage(BigDecimal lossPercentage) {
-        this.lossPercentage = lossPercentage;
-    }
-
-    public BigDecimal getNetSalesPrice() {
-        return netSalesPrice;
-    }
-
-    public void setNetSalesPrice(BigDecimal netSalesPrice) {
-        this.netSalesPrice = netSalesPrice;
-    }
-
-    public BigDecimal getGrossPurchasePrice() {
-        return grossPurchasePrice;
-    }
-
-    public void setGrossPurchasePrice(BigDecimal grossPurchasePrice) {
-        this.grossPurchasePrice = grossPurchasePrice;
-    }
 }
