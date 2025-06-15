e406f79f2ea953001857920de6f514af8255e807
Gustavo <gu.alvesslv@gmail.com>
2025-06-15 19:34:54 -0300

    :bug: fix: Atualiza validacoes e mensagens do SpecieRequestDTO Ajusta mensagens e limites de validacao nos campos DTO

--- src/main/java/com/example/scpv/specie/dto/SpecieRequestDTO.java
+++ src/main/java/com/example/scpv/specie/dto/SpecieRequestDTO.java
@@ -6,21 +6,21 @@ import java.math.BigDecimal;
 
 public record SpecieRequestDTO(
 
-        @NotBlank(message = "Name can't be null.")
-        @Size(min = 3, max = 100)
+        @NotBlank(message = "Species name cannot be blank.")
+        @Size(min = 3, max = 100, message = "Species name must be between 3 and 100 characters.")
         String speciesName,
 
-        @NotNull(message = "Loss percentage can't be null.")
-        @DecimalMin(value = "0.0", message = "Loss percentage can't be negative.")
-        @DecimalMax(value = "100.0", message = "Loss percentage can't be more than 100.")
+        @NotNull(message = "Loss percentage cannot be null.")
+        @DecimalMin(value = "0.0", message = "Loss percentage cannot be negative.")
+        @DecimalMax(value = "100.0", message = "Loss percentage cannot exceed 100.0.")
         BigDecimal lossPercentage,
 
-        @NotNull(message = "Net sales price can't be null.")
-        @DecimalMin(value = "0.1", message = "Net sales price can't be negative.")
+        @NotNull(message = "Net sales price cannot be null.")
+        @DecimalMin(value = "0.01", message = "Net sales price must be greater than zero.")
         BigDecimal netSalesPrice,
 
-        @NotNull(message = "Gross purchase price can't be null.")
-        @DecimalMin(value = "0.1", message = "Gross purchase price can't be negative.")
+        @NotNull(message = "Gross purchase price cannot be null.")
+        @DecimalMin(value = "0.01", message = "Gross purchase price must be greater than zero.")
         BigDecimal grossPurchasePrice
 ) {
 }
